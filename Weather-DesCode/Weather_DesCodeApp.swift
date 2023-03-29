3afa86f9b2a5bac19884ef8f113206c89fffc38a
Brian McIntosh <brian@mixinteractive.com>
2023-03-29 15:21:52 -0400

    Initial Commit

---
+++ Weather-DesCode/Weather_DesCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_DesCodeApp.swift
+//  Weather-DesCode
+//
+//  Created by Brian McIntosh on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_DesCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
